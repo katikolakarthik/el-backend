3762b1361d42d8ed7d9ec166e84981f83fd3596d
KATIKOLA KARTHIK <154609029+katikolakarthik@users.noreply.github.com>
2025-08-10 19:25:23 +0530

    Update Assignment.js

--- models/Assignment.js
+++ models/Assignment.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
-const assignmentSchema = new mongoose.Schema({
-  moduleName: { type: String, required: true },
-  subModuleName: String, // optional
+const subAssignmentSchema = new mongoose.Schema({
+  subModuleName: { type: String, required: true },
   patientName: String,
   icdCodes: [String],
   cptCodes: [String],
@@ -15,6 +14,13 @@ const assignmentSchema = new mongoose.Schema({
     notes: String
   },
   assignedStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+  assignedDate: { type: Date, default: Date.now }
+});
+
+const assignmentSchema = new mongoose.Schema({
+  moduleName: { type: String, required: true },
+  subAssignments: [subAssignmentSchema], // multiple sub-assignments
+  assignedStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }], // if assigning whole module
   assignedDate: { type: Date, default: Date.now }
 });
 
