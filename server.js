5e5b63615de1bfea9a53b12c9aaf1b0846ca5608
karthik <katikolakarthik@gmail.com>
2025-08-17 17:02:33 +0530

    Update CORS to allow localhost only

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(
       const allowedOrigins = [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:3000",
-        "https://el-front-umber.vercel.app"
+        "http://localhost:3000"
       ];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
