ca612f12f058f4a882c3180e6558004e8f0768d6
KATIKOLA KARTHIK <katikolakarthik@gmail.com>
2025-08-23 22:18:22 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -7,8 +7,14 @@ const studentSchema = new mongoose.Schema({
   paidAmount: Number,
   remainingAmount: Number,
   enrolledDate: Date,
+  expiryDate: { type: Date, required: true }, // Add expiry date field
   profileImage: String,
-  role: { type: String, default: "user" }
+  role: { type: String, default: "user" },
+  // Add a field that will be used for TTL index
+  expiresAt: { type: Date, index: { expireAfterSeconds: 0 } }
 });
+
+// Create a TTL index on expiresAt field
+studentSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = mongoose.model("Student", studentSchema);
